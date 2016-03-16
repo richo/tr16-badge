502898ed004906b507196d65835c1c09a06ebbae
sergej.schmidt <sergej@violentinter.net>
2016-03-16 01:07:51 +0100

    Working python script for radio.

--- contiki3/examples/tr16-badge/events_prototypes.py
+++ contiki3/examples/tr16-badge/events_prototypes.py
@@ -1,6 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
-import collections
+from rflib import *
+from time import sleep
+
+d = RfCat()
+d.setFreq(868000000)
+d.makePktFLEN(30)
+d.setMdmModulation(MOD_GFSK)
+d.setMdmSyncMode(SYNCM_30_of_32)
+d.setMdmSyncWord(0xaabb)
+d.setMdmDRate(50000)
+d.setMdmDeviatn(25000)
+d.setEnablePktCRC()
+
+def send_data(data):
+    data = str(data)
+    for i in range(0,80):
+        d.RFxmit(data)
+        sleep(0.5)
+
 
 days = {
         "day1" : 0x00, 
@@ -22,7 +40,12 @@ events = {
 print("WARNING: day2 only goes from 0 - 5 slots (too lazy to make proper loops)")
 print('---------------')
 print("This can be arbitrary text(limited to 0xff+ 27 bytes) sent over the radio: ")
-print(hex(0xFF) + 'A' * 27)
+msg = chr(0xFF) + 'WOOF' * 4  
+print(msg)
+send_data(msg)
+#d.setModeTX()
+#d.cleanup()
+#exit(1)
 print('---------------')
 for key, msg_code in events.items():
     if msg_code == 0x05:
@@ -30,25 +53,36 @@ for key, msg_code in events.items():
         for slot in range(0x00, 0x80, 0x10): # 8 talks from 0x00 to including 0x70 
             msg  = msg_code | days['day1'] | slot
             print('\t--5min. day1 {0} slot: {1} msg: {2}'.format(key, slot, hex(msg)) )
+            send_data(chr(msg))
             msg  = msg_code | days['day2'] | slot
             print('\t--5min. day2 {0} slot: {1} msg: {2}'.format(key, slot, hex(msg)) )
+            send_data(chr(msg))
             print('\t--------------------------')
     elif msg_code == 0x06:
         print("Next round of talks in 10min: ")
         for slot in range(0x00, 0x80, 0x10): # 8 talks from 0x00 to including 0x70 
             msg  = msg_code | days['day1'] | slot
             print('\t--5min. day1 {0} slot: {1} msg: {2}'.format(key, slot, hex(msg)) )
+            send_data(chr(msg))
             msg  = msg_code | days['day2'] | slot
             print('\t--5min. day2 {0} slot: {1} msg: {2}'.format(key, slot, hex(msg)) )
+            send_data(chr(msg))
             print('\t--------------------------')
     elif msg_code == 0x07:
         print("Wanky messages: ")
         msg = msg_code | 0x00 
         print('\tWanky message: nicebadge: msg: {0}'.format(hex(msg)) )
+        send_data(chr(msg))
         msg = msg_code | 0x10 
         print('\tWanky message: angry botnet: msg: {0}'.format(hex(msg)) )
+        send_data(chr(msg))
         msg = msg_code | 0x20 
         print('\tWanky message: : msg: {0}'.format(hex(msg)) )
+        send_data(chr(msg))
     else:
         print( 'event: {0} msg: {1}'.format(key,hex(msg_code)) )
+        send_data(chr(msg))
+
     print('=================')
+d.setModeTX()
+d.cleanup()
